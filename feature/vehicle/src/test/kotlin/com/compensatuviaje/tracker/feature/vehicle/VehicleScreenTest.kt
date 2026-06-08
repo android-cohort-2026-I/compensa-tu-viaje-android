2c5b686a74132103275a71adec8b9acd32b0af5b
luisberrio-lang <luis.berrio@tecsup.edu.pe>
2026-06-08 14:05:04 -0500

    test(vehicle): Screen expone datos del camion

--- feature/vehicle/src/test/kotlin/com/compensatuviaje/tracker/feature/vehicle/VehicleScreenTest.kt
+++ feature/vehicle/src/test/kotlin/com/compensatuviaje/tracker/feature/vehicle/VehicleScreenTest.kt
@@ -1,10 +1,20 @@
 package com.compensatuviaje.tracker.feature.vehicle
 
+import com.compensatuviaje.tracker.testing.SampleData
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 
 class VehicleScreenTest {
-    @Test fun `placeholder passes`() {
-        assertThat(true).isTrue()
+    @Test
+    fun `content state exposes vehicle data`() {
+        val state = VehicleUiState.Content(
+            session = SampleData.session,
+            truck = SampleData.truck,
+            hasActiveTrip = false,
+            isConfirming = false,
+        )
+
+        assertThat(state.truck.licensePlate).isEqualTo("XYZ-987")
+        assertThat(state.truck.category).isEqualTo("heavy_duty")
     }
 }
